9e35d3adc58b644cce0612b2ab6312ce4add0aa4
harrislachlan <58382461+harrislachlan@users.noreply.github.com>
2021-01-20 11:39:38 +0000

    Update 03_hoch_diff_gene.R

--- scripts/03_hoch_diff_gene.R
+++ scripts/03_hoch_diff_gene.R
@@ -1,17 +1,16 @@
 load("hoch_nsc_lineage.RData")
 
-hoch_nsc_lineage <- NormalizeData(hoch_nsc_lineage)
 DefaultAssay(hoch_nsc_lineage) <- "RNA"
-A <- FindMarkers(hoch_nsc_lineage, ident.1 = "nsc", min.diff.pct = 0.0, ident.2 = "granule neurons", 
-                 min.pct = 0.1, assay = "RNA", logfc.threshold = 0.25)
-
-#determine no genes to correct p-value
-length(rownames(hoch_nsc_lineage@assays$RNA@data))
+hoch_nsc_lineage <- NormalizeData(hoch_nsc_lineage, assay = "RNA")
+A <- FindMarkers(hoch_nsc_lineage, ident.1 = "nsc", ident.2 = "granule neurons", 
+                 min.pct = 0.0, assay = "RNA", logfc.threshold = 0.0)
 
 sig_genes_pvalue <- A$p_val
-FDR <- p.adjust(sig_genes_pvalue, "fdr", n = 14545)
+FDR <- p.adjust(sig_genes_pvalue, "fdr")
 A$p_val_adj <- FDR
+A <- A %>% filter(pct.1 >= 0.1|pct.2 >= 0.1)
 
 #granule neurons here are a mix of neuroblasts and more mature neurons
 write.csv(A, "nscs_neurob.csv")
+
 
